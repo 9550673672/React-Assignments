72b3728063bc3cf92a10bba045d4879d3d544dd2
vishnuvardhan <vishnumech99@gmail.com>
2022-04-07 21:38:10 +0530

    changes made on app css and js

--- Assignment-2/code/my-app/src/App.js
+++ Assignment-2/code/my-app/src/App.js
@@ -1,18 +1,30 @@
-import logo from './logo.svg';
+// import './index.css';
 import './App.css';
+import './index.js';
 
-
-
-function App(props) {
- const {employee}=props;
-    
-return(
-  <div className="container">
-  {/* Start your React code here */}
-</div>
-)
+const employee = {
+    name: 'Tom',
+    location: 'New York',
+    bloodGroup: 'B+',
+    age: 28,
+    profileImg: 'https://www.w3schools.com/howto/img_avatar.png'
 }
 
-
-
+function data(props){
+  return(
+  <div className="container">
+      <img className='img' src={props.profileImg} alt="logo"/>
+      <h4 className='name'>{props.name}</h4>
+  <div className='details'>
+      <h4>Location</h4>
+      <p>{props.location}</p>
+      <h4>Blood Group</h4>
+      <p>{props.bloodGroup}</p>
+      <h4>Age</h4>
+      <p>{props.age}</p>
+  </div>  
+  </div>
+);
+}
+let App=()=>data(employee)
 export default App;
